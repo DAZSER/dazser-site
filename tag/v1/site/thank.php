24fcba1c3416683ae61c8b19fa2a4451f822fffb
Sparticuz <kyle@kmcnally.net>
2010-08-23 17:06:20 +0000

    remove all trunk, tag, and branch folders

--- tag/v1/site/thank.php
+++
@@ -1,5 +0,0 @@
-<link href="../main.css" rel="stylesheet" type="text/css" />
-<p><img src="../images/logo.jpg" width="301" height="146"><br>
-  Thank You for completing the Contact form. You will be contacted within one business day.</p>
-<p>&nbsp;</p>
-<p><a href="../index.htm">Please click here to return to the main Jani-King Site. </a></p>
--- tag/v2/assets/opendb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'root';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
-$dbname = 'janiking';
-mysql_select_db($dbname) or die('Error connecting to db');
-?>
