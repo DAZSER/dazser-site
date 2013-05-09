990d4966d2ec043ec8f8176fd46a7f56f2c85d3f
Sparticuz <kyle@kmcnally.net>
2013-05-09 18:47:05 +0000

    Updates

--- assets/offices_process.php
+++ assets/offices_process.php
@@ -13,12 +13,12 @@ $redpill = '<div style="float:left;width:280px;">';
 $redpill .= '<address>';
 $redpill .= '<img src="/images/'.$location.'/name.png" class="png" alt="Jani-King of '.ucwords($location).'"/><br />';
 // get director's name
-$sql = "SELECT concat_ws(' ',FirstName,LastName) AS name, email_address FROM gen_employee WHERE ulink='".$row['RegionalDirector_fk']."'";
+$sql = "SELECT concat_ws(' ',FirstName,LastName) AS name, `email_address` FROM gen_employee WHERE ulink='".$row['RegionalDirector_fk']."'";
 $result = mysql_query($sql) or die('Query failed: '.mysql_error());
 $director = mysql_fetch_assoc($result);
 
 $redpill .= $director['name'].'<br />';
-$redpill .= '<a href="mailto:'.$row['email_address'].'">'.$row['email_address'].'</a><br />';
+$redpill .= '<a href="mailto:'.$director['email_address'].'">'.$director['email_address'].'</a><br />';
 $redpill .= '<a href=\'http://maps.google.com/maps?q=%22'.$row['AddressLine1'].', '.$row['AddressCity'].', '.$row['AddressState'].'%22\'>'.$row['AddressLine1'].'<br />';
 if ($row['AddressLine2'] != "") {
 	$redpill .= $row['AddressLine2'].'<br/>';
@@ -31,7 +31,7 @@ $redpill .= '</address>';
 $redpill .= '</div>';
 
 $bluepill = '<div style="float:left;width:280px;text-align:center;">';
-$bluepill .= '<img src="/images/'.$location.'/director.jpg" alt="'.$row['director'].', Regional Director" />';
+$bluepill .= '<img src="/images/'.$location.'/director.jpg" alt="'.$director['name'].', Regional Director" />';
 $bluepill .= '</div>';
 
 include 'closedb.php';
